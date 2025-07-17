814d8cbc29f244769a4f904f1bbe3be61ea7a827
Yosua <yosuammwowor@gmail.com>
2025-07-17 12:35:20 +0800

    add: hero section, profil. fix: hero section, responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         primary: "#FE7743",
         dark: "#0f172a",
         secondary: "#64748b",
+      },
+      screens: {
+        xl: "1320px",
       },
     },
   },
