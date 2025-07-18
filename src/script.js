0beb4848272762b1038efe783bed8c924597217d
Yosua <yosuammwowor@gmail.com>
2025-07-18 10:09:35 +0800

    fix: navigation bar, hamburger menu links

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
+// Navigation bar
+const navMenu = document.querySelector("#nav-menu");
+
 // Hamburger bar
 const hamburger = document.querySelector("#hamburger");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
