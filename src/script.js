5c55227156f72955600d911d6ce6d625023655e3
Yosua <yosuammwowor@gmail.com>
2025-07-18 11:06:13 +0800

    fix: navigation bar, style

--- src/script.js
+++ src/script.js
@@ -1,3 +1,15 @@
+// Navbar Transparent
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  const topScreen = window.pageYOffset;
+
+  if (topScreen > header.offsetTop) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+});
+
 // Navigation bar
 const navMenu = document.querySelector("#nav-menu");
 
